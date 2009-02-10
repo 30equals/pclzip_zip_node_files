ac198e7f5cf14eb955d68aab192ea65fe470d5b4
ssm2017 Binder <ssm2017_binder@283775.no-reply.drupal.org>
2009-02-10 09:06:06 +0000

    Initial deposit

---
+++ download_link.tpl.php
@@ -0,0 +1,3 @@
+<?php // $Id$ ?>
+<?php global $base_url; $image_path = drupal_get_path('module', 'pclzip_zip_node_files'); ?>
+<img alt="<?php print t('Get attached files'); ?>" title="<?php print t('Get attached files'); ?>" src="<?php print $base_url; ?>/<?php print $image_path; ?>/images/package-x-generic.png" />
